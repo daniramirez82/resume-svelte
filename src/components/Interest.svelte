3f9f3b07d5b3852056227a86821b790de374da88
Dani <dani.ramirez.madrid@gmail.com>
2022-06-19 14:25:06 +0200

    version 1 all ready!!

--- src/components/Interest.svelte
+++ src/components/Interest.svelte
@@ -2,24 +2,40 @@
 </script>
 
 <section class="resume-section" id="interests">
-	<h2>Interests</h2>
-	<div class="resume-section-content">
-		<h5 class="mb-5">What I want to learn this year</h5>
-		<p>
-			Bealive it or not i start to learn Svelte before any other framework, i read about Svelte and
-			thoght it ill be a good start in the framework's world, so one of the fist thing i want to
-			learn this year is React. I would like to learn a nice anitamted library sush Gasap. And last
-			but not lease im very interesting in UX research.
-		</p>
-		<h5 class="mb-5">Interests</h5>
-		<p>
-			Basically sports, in my free time i love to practice racquets sports such padel, frontenis and
-			i'm an enthusiastic biker.
-		</p>
-		<p class="mb-0">
-			I like to construct a lot to stuff so i usually made some DIY home project with wood, and i
-			use to weld too.
-		</p>
+	<div class="interest-cont">
+		<h2>Interests</h2>
+		<div class="resume-section-content">
+			<h5 class="mb-5">What I want to learn this year</h5>
+			<p>
+				Believe it or not I start to learn Svelte before any other framework, I read about Svelte
+				and thought it will be a good start in the framework's world, so one of the first thing I
+				want to learn this year is React. I would like to learn a nice animated library such Gasap.
+				And last but not lease, I'm very interested in UX research.
+			</p>
+			<h5 class="mb-5">Interests</h5>
+			<p>
+				Basically sports, in my free time i love to practice racquets sports such padel, frontenis
+				and i'm an enthusiastic biker.
+			</p>
+			<p class="mb-0">
+				I like to construct a lot to stuff so i usually made some DIY home project with wood, and i
+				use to weld too.
+			</p>
+		</div>
+	</div>
+	<div class="footer">
+		<div class="footer-title">
+			<h2>THANKS FOR READ!</h2>
+		</div>
+		<div class="row">
+			<p><span>Please remember, if</span> <br /> <span>you want contact me!</span></p>
+			<div class="icons">
+				<a class="social-icon" href="#!"><i class="fab fa-linkedin-in" /></a>
+				<a class="social-icon" href="#!"><i class="fab fa-github" /></a>
+				<a class="social-icon" href="#!"><i class="fab fa-twitter" /></a>
+				<a class="social-icon" href="#!"><i class="fab fa-facebook-f" /></a>
+			</div>
+		</div>
 	</div>
 </section>
 
@@ -28,7 +44,46 @@
 		margin-bottom: 4rem;
 	}
 
+	.interest-cont {
+		padding: 48px;
+	}
+
 	.resume-section#interests {
 		flex-direction: column;
+		justify-content: space-between;
+		padding: 0 0 0 0;
 	}
+	.footer {
+		background-color: #343a40;
+		color: whitesmoke;
+		width: 100%;
+	}
+
+	.row p {
+		width: auto;
+	}
+
+	.row .icons {
+		width: auto;
+	}
+
+	.row {
+		display: flex;
+        justify-content: center;
+        align-items: flex-end;
+	}
+
+	.icons {
+		margin-bottom: 2rem;
+        width: auto;
+	}
+
+    .footer-title{
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        padding-top: 2rem;
+    }
+
+
 </style>
